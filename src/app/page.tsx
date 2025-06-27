8fddd2322375c21046d612b171079cac131dfa0c
tomsarpo <t.j.sarpola@gmail.com>
2025-06-28 01:10:21 +0300

    Fix Firebase Configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  redirect('/login');
+  redirect('/en/login');
   // The return value is not used because redirect() will interrupt rendering.
   // However, to satisfy TypeScript and ESLint, a return statement is needed.
   return null;
